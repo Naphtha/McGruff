43df0a63770ecf86b0c9425398a73c5286c04e58
Shawn Shojaie <shojaie.mac@gmail.com>
2014-01-26 15:16:56 -0800

    Tempfile now contains split lines of activity.

--- Parser.py
+++ Parser.py
@@ -1,19 +1,30 @@
 
-
+from HTMLParser import HTMLParser
 
 f = open("log.html", 'r')
 tempfile = open("tempLog.tmp", 'w')
 line = ""
+flag = 0
 
 for line in f:
 
-	if "Daily Activity Log" in line:
+	if "<h1>Daily Activity Log " in line:
 		print line
-
-	if "</article></div>" in line:
+		tempfile.write(line)
+		
+	if "*==========================================================================" in line:
 		print line
-
+		line = line.replace("*==========================================================================", "\n\n")
+		tempfile.write(line)
 
 f.close()
 tempfile.close()
 
+# tempfile = open("tempLog.tmp", 'r')
+
+# for line in tempfile:
+
+# 	for word in line:
+
+# 		print word
+# tempfile.close()
