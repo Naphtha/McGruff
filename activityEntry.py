a5ded8db9450bd981d25ed0480a29755bf106b20
Shawn Shojaie <shojaie.mac@gmail.com>
2014-12-08 00:51:17 -0800

    Add pairwise iteration. Update ActivityEntry Class. Create ActivityEntry for each event.

--- activityEntry.py
+++ activityEntry.py
@@ -1,12 +1,13 @@
-class ActivityEntry():
+import re
 
+class ActivityEntry():
 
 	#unformatted entry is the HTML line that comes direct from parser
 
 	def __init__(self, time, desc):
 
 		self.time = time
-		temp = desc.split('<p>')
-		print temp
+		temp = re.split('</*p>', desc)
+		print temp[0]
 
 		
--- newparser.py
+++ newparser.py
@@ -1,5 +1,10 @@
 import re
-import ActivityEntry.py
+
+from ActivityEntry import ActivityEntry
+
+from utils import pairwise
+
+
 
 f = open("log.html", 'r')
 
@@ -15,9 +20,21 @@ f.close()
 
 time_re = re.compile('(\d\d:\d\d)')
 
-for time, item in time_re.split(line):
+split_log = time_re.split(line)
 
-	print '\n'
+if re.match( '\s*<table' ,split_log[0]):
+	split_log.pop(0)
+
+
+for time, event in pairwise(split_log):
+	ActivityEntry(time, event)
 
 tempfile = open("tempLog2.tmp", 'w')
 tempfile.close()
+
+
+
+
+
+
+
